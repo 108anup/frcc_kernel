5ea06b20d466bb788ac988dbf30b8ec177cf7775
Anup Agarwal <anupa@andrew.cmu.edu>
2025-02-10 23:43:05 +0000

    Checkpoint

--- tcp_ndd.c
+++ tcp_ndd.c
@@ -7,21 +7,25 @@ NDD: A provably fair and robust congestion controller
 
 #define NDD_DEBUG
 
-#define P_SCALE 8 /* scaling factor for fractions in BBR (e.g. gains) */
+#define P_SCALE 8 /* scaling factor for fractions (e.g. gains) */
 #define P_UNIT (1 << P_SCALE)
 
+// Assumptions about network scenarios
 static const u32 p_ub_rtprop_us = 100000; // 100 ms
 static const u32 p_ub_rtterr_us = 10000; // 10 ms
-static const u32 p_contract_min_qdel_us = p_ub_rtprop_us / 2;
+static const u32 p_ub_flow_count = 10;
 
+// Design parameters
 // TODO: check if these floating values make sense given the UNIT. Should we
 // change the unit?
+static const u32 p_contract_min_qdel_us = p_ub_rtprop_us / 2;
+static const u32 p_probe_duration_us =
+	p_ub_rtterr_us; // ? Do we want something else here?
+static const u32 p_probe_multiplier_unit = P_UNIT * 4;
 static const u32 p_cwnd_averaging_factor_unit = P_UNIT * 1 / 2;
 static const u32 p_cwnd_clamp_hi_unit = P_UNIT * 6 / 5;
 static const u32 p_cwnd_clamp_lo_unit = P_UNIT * 11 / 10;
-static const u32 p_probe_multiplier_unit = P_UNIT * 4;
-static const u32 p_probe_duration = p_ub_rtterr_us;
-// ? Do we want something else here?
+static const u32 p_slot_load_factor_unit = P_UNIT * 2;
 
 static u32 id = 0;
 struct ndd_data {
@@ -30,14 +34,23 @@ struct ndd_data {
 	// State variables
 	u64 s_min_rtprop_us;
 
+	u32 s_round_slots_till_now;
 	u32 s_round_min_rtt_us;
 	u32 s_round_max_rate_pps; // packets per second (1500 bytes per sec to 6.44 Terabytes per second)
+
 	u32 s_slot_max_qdel_us;
+	u64 s_slot_start_time_us;
 
 	bool s_probe_ongoing;
 	u32 s_probe_min_excess_delay_us;
 	u32 s_probe_prev_cwnd_pkts;
 	u32 s_probe_excess_pkts;
+
+	u32 s_probe_start_seq;
+	u32 s_probe_inflightmatch_seq;
+	u32 s_probe_first_seq;
+	u32 s_probe_last_seq;
+	u64 s_probe_first_seq_snd_time;
 };
 
 static void ndd_init(struct sock *sk)
@@ -61,17 +74,54 @@ static bool ndd_valid(struct ndd_data *ndd)
 	return (ndd);
 }
 
+// TODO: Check what sequence numbers to use
+static u32 get_last_snd_seq(struct tcp_sock *tsk)
+{
+	return tsk->data_segs_out;
+}
+
+static u32 get_last_rcv_seq(struct tcp_sock *tsk)
+{
+	return tsk->data_segs_in;
+}
+
 static bool part_of_probe(struct ndd_data *ndd, struct tcp_sock *tsk)
 {
+	u64 last_rcv_seq = get_last_rcv_seq(tsk);
+	if (ndd->s_probe_start_seq > 0) {
+		if (ndd->s_probe_last_seq > 0) {
+			return !before(last_rcv_seq, ndd->s_probe_start_seq) &&
+			       !after(last_rcv_seq, ndd->s_probe_last_seq);
+		} else {
+			return !before(last_rcv_seq, ndd->s_probe_start_seq);
+		}
+	}
 	return false;
+}
+
+static u32 get_initial_rtt(struct tcp_sock *tsk)
+{
+	// TODO: Should we be calling this every packet?
+	// Get initial RTT - as measured by SYN -> SYN-ACK.  If information
+	// does not exist - use U32_MAX
+	u32 init_rtt_us;
+	if (tsk->srtt_us) {
+		init_rtt_us = max_t(u32, tsk->srtt_us >> 3, 1U);
+	} else {
+		init_rtt_us = U32_MAX;
+	}
+	return init_rtt_us;
 }
 
 static void update_estimates(struct ndd_data *ndd, struct tcp_sock *tsk,
 			     const struct rate_sample *rs, u32 rtt_us)
 {
 	u32 this_qdel = rtt_us - ndd->s_min_rtprop_us;
-	u64 this_rate_pps = tsk->snd_cwnd * USEC_PER_SEC / rtt_us;
+	u32 init_rtt_us = get_initial_rtt(tsk);
+	u64 this_rate_pps = tsk->snd_cwnd * USEC_PER_SEC;
+	do_div(this_rate_pps, rtt_us);
 
+	ndd->s_min_rtprop_us = min_t(u32, ndd->s_min_rtprop_us, init_rtt_us);
 	ndd->s_min_rtprop_us = min_t(u32, ndd->s_min_rtprop_us, rtt_us);
 	ndd->s_round_min_rtt_us = min_t(u32, ndd->s_round_min_rtt_us, rtt_us);
 
@@ -89,43 +139,42 @@ static void update_estimates(struct ndd_data *ndd, struct tcp_sock *tsk,
 	}
 }
 
-static void reset_round_estimates(struct ndd_data *ndd)
+static void reset_round_state(struct ndd_data *ndd)
 {
+	ndd->s_round_slots_till_now = 0;
 	ndd->s_round_min_rtt_us = U32_MAX;
 	ndd->s_round_max_rate_pps = 0;
 }
 
-static u32 get_initial_rtt(struct tcp_sock *tsk)
+static bool probe_ended(struct ndd_data *ndd, struct tcp_sock *tsk)
 {
-	// Get initial RTT - as measured by SYN -> SYN-ACK.  If information
-	// does not exist - use U32_MAX as RTT
-	u32 rtt_us;
-	if (tsk->srtt_us) {
-		rtt_us = max_t(u32, tsk->srtt_us >> 3, 1U);
-	} else {
-		rtt_us = U32_MAX;
-	}
-	return rtt_us;
+	u64 last_rcv_seq = get_last_rcv_seq(tsk);
+	return ndd->s_probe_last_seq > 0 && after(last_rcv_seq, ndd->s_probe_last_seq);
 }
 
-static bool probe_ended(struct ndd_data *ndd, struct tcp_sock *tsk, u32 rtt_us)
+static bool cruise_ended(struct ndd_data *ndd, u64 now_us)
 {
-	return false;
-}
-
-static bool cruise_ended(struct ndd_data *ndd, struct tcp_sock *tsk, u32 rtt_us)
-{
-	return false;
+	u64 slot_duration_us = 3 * (ndd->s_slot_max_qdel_us + p_ub_rtprop_us) +
+			       p_probe_duration_us;
+	u64 slot_end_us = ndd->s_slot_start_time_us + slot_duration_us;
+	return time_after64(now_us, slot_end_us);
 }
 
 static bool should_init_probe_end(struct ndd_data *ndd, struct tcp_sock *tsk)
 {
-	return false;
+	u32 last_snd_seq = get_last_snd_seq(tsk);
+	return ndd->s_probe_last_seq >= 0 &&
+	       !before(last_snd_seq, ndd->s_probe_last_seq);
 }
 
 static bool round_ended(struct ndd_data *ndd, struct tcp_sock *tsk)
 {
-	return false;
+	u64 slots_per_round;
+	slots_per_round = p_ub_flow_count;
+	slots_per_round *= p_slot_load_factor_unit;
+	slots_per_round <<= P_SCALE;
+
+	return ndd->s_round_slots_till_now >= slots_per_round;
 }
 
 static bool should_probe(struct ndd_data *ndd, struct tcp_sock *tsk)
@@ -138,6 +187,11 @@ static void reset_probe_state(struct ndd_data *ndd)
 	ndd->s_probe_ongoing = false;
 	ndd->s_probe_min_excess_delay_us = U32_MAX;
 	ndd->s_probe_prev_cwnd_pkts = 0; // should not be read anyway.
+
+	ndd->s_probe_start_seq = 0;
+	ndd->s_probe_inflightmatch_seq = 0;
+	ndd->s_probe_first_seq = 0;
+	ndd->s_probe_last_seq = 0;
 }
 
 static u32 get_target_flow_count_unit(struct ndd_data *ndd)
@@ -148,7 +202,7 @@ static u32 get_target_flow_count_unit(struct ndd_data *ndd)
 	round_qdel_us = ndd->s_round_min_rtt_us - ndd->s_min_rtprop_us;
 	target_flow_count_unit = P_UNIT;
 	target_flow_count_unit *= round_qdel_us;
-	target_flow_count_unit = do_div(target_flow_count_unit, p_contract_min_qdel_us);
+	do_div(target_flow_count_unit, p_contract_min_qdel_us);
 
 	return target_flow_count_unit;
 }
@@ -172,12 +226,53 @@ static void start_probe(struct ndd_data *ndd, struct tcp_sock *tsk)
 	excess_pkts = max_t(u64, excess_pkts, 1);
 	ndd->s_probe_excess_pkts = excess_pkts;
 
+	ndd->s_probe_start_seq = get_last_snd_seq(tsk);
+	ndd->s_probe_inflightmatch_seq = 0;
+	ndd->s_probe_first_seq = 0;
+	ndd->s_probe_last_seq = 0;
+
 	ndd->s_probe_prev_cwnd_pkts = tsk->snd_cwnd;
 	tsk->snd_cwnd += excess_pkts;
 }
 
+static void start_new_slot(struct ndd_data *ndd, u64 now_us)
+{
+	ndd->s_slot_max_qdel_us = 0;
+	ndd->s_slot_start_time_us = now_us;
+}
 
-static void update_cwnd(struct ndd_data *ndd, struct tcp_sock *tsk)
+static void update_probe_state(struct ndd_data *ndd, struct tcp_sock *tsk,
+			       const struct rate_sample *rs, u64 now_us)
+{
+	u64 last_rcv_seq;
+	u64 last_snd_seq;
+	u64 end_seq_snd_time;
+
+	last_rcv_seq = get_last_rcv_seq(tsk);
+	last_snd_seq = get_last_snd_seq(tsk);
+	// TODO: check if we should use last_snd_seq or last_snd_seq + 1
+
+	if (ndd->s_probe_inflightmatch_seq == 0) {
+		// TODO: currently assuming the inflight match will happen
+		// within 1 RTT, ideally just check inflight = cwnd.
+		if (after(last_rcv_seq, ndd->s_probe_start_seq)) {
+			ndd->s_probe_inflightmatch_seq = last_snd_seq;
+		}
+	}
+	else if (ndd->s_probe_first_seq == 0) {
+		if (after(last_rcv_seq, ndd->s_probe_inflightmatch_seq)) {
+			ndd->s_probe_first_seq = last_snd_seq;
+			ndd->s_probe_first_seq_snd_time = now_us;
+		}
+	} else if (ndd->s_probe_last_seq == 0) {
+		end_seq_snd_time = ndd->s_probe_first_seq_snd_time + p_probe_duration_us;
+		if (time_after64(now_us, end_seq_snd_time)) {
+			ndd->s_probe_last_seq = last_snd_seq;
+		}
+	}
+}
+
+static void update_cwnd(struct sock* sk, struct ndd_data *ndd, struct tcp_sock *tsk, u32 rtt_us)
 {
 	u64 bw_estimate_pps;
 	u64 flow_count_belief_unit;
@@ -186,15 +281,15 @@ static void update_cwnd(struct ndd_data *ndd, struct tcp_sock *tsk)
 	u64 fc_belief_lo_clamp;
 	u64 target_cwnd_unit;
 	u64 next_cwnd_unit;
+	u64 next_rate_bps;
 
 	bw_estimate_pps = ndd->s_probe_excess_pkts;
 	bw_estimate_pps *= USEC_PER_SEC;
-	bw_estimate_pps =
-		do_div(bw_estimate_pps, ndd->s_probe_min_excess_delay_us);
+	do_div(bw_estimate_pps, ndd->s_probe_min_excess_delay_us);
 
 	flow_count_belief_unit = P_UNIT;
 	flow_count_belief_unit *= bw_estimate_pps;
-	flow_count_belief_unit = do_div(flow_count_belief_unit, ndd->s_round_max_rate_pps);
+	do_div(flow_count_belief_unit, ndd->s_round_max_rate_pps);
 
 	target_flow_count_unit = get_target_flow_count_unit(ndd);
 	fc_belief_hi_clamp =
@@ -214,15 +309,19 @@ static void update_cwnd(struct ndd_data *ndd, struct tcp_sock *tsk)
 
 	target_cwnd_unit = tsk->snd_cwnd << P_SCALE;
 	target_cwnd_unit *= flow_count_belief_unit;
-	target_cwnd_unit = do_div(target_cwnd_unit, target_flow_count_unit);
+	do_div(target_cwnd_unit, target_flow_count_unit);
 
 	next_cwnd_unit =
 		(1 - p_cwnd_averaging_factor_unit) * tsk->snd_cwnd +
 		((p_cwnd_averaging_factor_unit * target_cwnd_unit) >> P_SCALE);
 	tsk->snd_cwnd = DIV_ROUND_UP_ULL(next_cwnd_unit, P_UNIT);
+
+	next_rate_bps = 2 * tsk->snd_cwnd * ndd_get_mss(tsk) * USEC_PER_SEC;
+	do_div(next_rate_bps, rtt_us);
+	sk->sk_pacing_rate = next_rate_bps;
 }
 
-static void ndd_cong_ctrl(struct sock *sk, const struct rate_sample *rs)
+static void on_ack(struct sock *sk, const struct rate_sample *rs)
 {
 	struct ndd_data *ndd = inet_csk_ca(sk);
 	struct tcp_sock *tsk = tcp_sk(sk);
@@ -238,23 +337,31 @@ static void ndd_cong_ctrl(struct sock *sk, const struct rate_sample *rs)
 	rtt_us = rs->rtt_us;
 	update_estimates(ndd, tsk, rs, rtt_us);
 
+	if (ndd->s_probe_ongoing) {
+		update_probe_state(ndd, tsk, rs, now_us);
+	}
+
 	if (ndd->s_probe_ongoing && should_init_probe_end(ndd, tsk)) {
 		tsk->snd_cwnd = ndd->s_probe_prev_cwnd_pkts;
 	}
 
-	if (cruise_ended(ndd, tsk, rtt_us) || probe_ended(ndd, tsk, rtt_us)) {
+	if (cruise_ended(ndd, now_us) || probe_ended(ndd, tsk)) {
+		ndd->s_round_slots_till_now++;
+
+		// probe ended
 		if (ndd->s_probe_ongoing) {
 			reset_probe_state(ndd);
-			update_cwnd(ndd, tsk);
+			update_cwnd(sk, ndd, tsk, rtt_us);
 		}
 
 		if (round_ended(ndd, tsk)) {
-			reset_round_estimates(ndd);
+			reset_round_state(ndd);
 		}
 
-		if (should_probe(ndd, tsk)) {
+		if (ndd->s_round_slots_till_now >= 1 && should_probe(ndd, tsk)) {
 			start_probe(ndd, tsk);
 		}
+		start_new_slot(ndd, now_us);
 	}
 }
 
@@ -279,7 +386,7 @@ static struct tcp_congestion_ops tcp_ndd_cong_ops __read_mostly = {
 	.owner = THIS_MODULE,
 	.init = ndd_init,
 	.release = ndd_release,
-	.cong_control = ndd_cong_ctrl,
+	.cong_control = on_ack,
 	// Since NDD does reduce cwnd on loss. We use reno's undo method.
 	.undo_cwnd = tcp_reno_undo_cwnd,
 	// Slow start threshold will not exist
